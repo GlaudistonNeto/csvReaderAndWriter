a0216cc65da39196f77a2ac223ebcb866d1f5f5d
GNeto <glaudistonson@gmail.com>
2020-03-07 20:37:11 -0300

    added try...catch

--- Reader.js
+++ Reader.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
+const util = require('util');
+
+// var newFunctionWithPromise = util.promisify(oldFunctionWithCallback)
 
 class Reader{
-  Read(filepath){
-    fs.readFile(filepath, 'utf8', (err, data) => {
-      if(err){
-        console.log(err);
-      }else{
-        console.log(data);
-      }
-    });
 
+  constructor()  {
+    this.reader = util.promisify(fs.readFile);
   }
 
+  async Read(filepath){
+    try{
+      return await this.reader(filepath, 'utf8');
+    }catch(err){
+      return undefined;
+    }    
+  }
 }
 
 module.exports = Reader;
